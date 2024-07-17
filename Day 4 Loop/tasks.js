96b2de8fe255b397bd1f6f2ee71ad49a8732c132
Arman <armanar6712@gmail.com>
2024-07-17 12:49:23 +0530

    colmpleted all tasks of day 3

--- Day 4 Loop/tasks.js
+++ Day 4 Loop/tasks.js
@@ -27,4 +27,50 @@ function sumToN(n) {
   }
   return j;
 }
-console.log(sumToN(5));
+// console.log(sumToN(5));
+
+function reverseCount(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+// reverseCount(10);
+
+function doWhile(n) {
+  let i = 1;
+  do {
+    console.log(i);
+    i++;
+  } while (i <= n);
+}
+// doWhile(5)
+
+function printStarPattern(rows) {
+  for (let i = 1; i < rows; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) {
+      pattern += "*";
+    }
+    console.log(pattern);
+  }
+}
+
+// printStarPattern(5);
+
+function skipN(n) {
+  for (let i = 1; i <= 10; i++) {
+    if (i == n) continue;
+    console.log(i);
+  }
+}
+// skipN(5)
+
+function break5(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i == 5) break;
+    console.log(i);
+  }
+}
+// break5(10);
