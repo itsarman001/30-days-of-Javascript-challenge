86afec8707a22df3f1cd68d5de22f583cae6c5ed
Arman <armanar6712@gmail.com>
2024-07-28 20:46:30 +0530

    completed 8 task of Day 13 classes

--- Day_14_Classes/index.js
+++ Day_14_Classes/index.js
@@ -1,12 +1,13 @@
 // Task 1
 class Person {
-  constructor(name, age) {
-    this.name = name;
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.age = age;
   }
 
   greeting() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    return `Hello, my name is ${this.firstName} ${this.lastName} and I am ${this.age} years old.`;
   }
 
   updateAge(newAge) {
@@ -15,11 +16,21 @@ class Person {
   }
 
   static getGenericGreeting() {
-    return `Hello there, i am ${this.name} & i am ${this.age} old`;
+    return `Hello there, i am ${this.firstName} ${this.lastName} & i am ${this.age} old`;
+  }
+
+  get greet(){
+    return `Hi i am ${this.firstName} ${this.lastName}`
+  } 
+
+  set setFullName(name){
+    let [firstName, lastName] = name.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
 }
 
-const spongeBob = new Person("Sponge Bob", 34);
+const spongeBob = new Person("Sponge", "Bob", 34);
 console.log(spongeBob.greeting());
 
 // Task 2
@@ -28,9 +39,11 @@ console.log(spongeBob.age);
 
 // Task 3
 class Student extends Person {
-  constructor(name, age, studentID) {
-    super(name, age);
+  static studentCount = 0;
+  constructor(firstName, lastName, age, studentID) {
+    super(firstName, lastName, age);
     this.studentID = studentID;
+    Student.studentCount++;
   }
 
   getStudentID() {
@@ -38,16 +51,26 @@ class Student extends Person {
   }
 
   greeting() {
-    return `Hello, my name is ${this.name}, I am ${this.age} years old, and my student ID is ${this.studentID}.`;
+    return `Hello, my name is ${this.firstName} ${this.lastName}, I am ${this.age} years old, and my student ID is ${this.studentID}.`;
   }
 }
 
-const student = new Student("Vivek", 18, "v12345");
-console.log(student.studentID);
+const student1 = new Student("Vivek", 18, "v12345");
+console.log(student1.studentID);
 
 // Task 4
-console.log(student.greeting())
+console.log(student1.greeting())
 
 // Task 5
 // this.age giving undefined even with giving a default age to age
 console.log(Person.getGenericGreeting());
+
+// Task 6
+console.log(Student.studentCount);
+
+// Task 7
+console.log(spongeBob.greet);
+
+// Task 8
+spongeBob.setFullName = 'Your Name';
+console.log(spongeBob.greet);
